dff672feac5d7e08e98033f189d83a692ed5d4be
Hyohwak Lee <crewe1746@naver.com>
2022-06-28 16:24:56 +0900

    Example hello world app-2

--- example/hello-world/main.go
+++ example/hello-world/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"log"
-
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
+
+type App struct {
+	output *widget.Label
+}
+
+var myApp App
 
 func main() {
 	// initialize application instance
@@ -14,16 +20,28 @@ func main() {
 	// create master window
 	w := a.NewWindow("Hello, World!")
 
+	// make UIs
+	output, entry, btn := myApp.makeUI()
+
 	// set the content of window
-	w.SetContent(widget.NewLabel("Hello, world!"))
+	w.SetContent(container.NewVBox(output, entry, btn))
+
+	//
+	w.Resize(fyne.NewSize(500, 500))
 
 	// show the window and run application with infinite loop
 	w.ShowAndRun()
 
-	// excute when application is closed
-	tidy()
 }
 
-func tidy() {
-	log.Println("would tidy up")
+func (app *App) makeUI() (*widget.Label, *widget.Entry, *widget.Button) {
+	output := widget.NewLabel("Hello World!")
+	entry := widget.NewEntry()
+	btn := widget.NewButton("Enter", func() {
+		app.output.SetText(entry.Text)
+	})
+	btn.Importance = widget.HighImportance
+
+	app.output = output
+	return output, entry, btn
 }
