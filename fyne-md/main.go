ecae1612614c67e08b2d64fe023b1493b80b7c11
Hyohwak Lee <crewe1746@naver.com>
2022-06-28 18:31:10 +0900

    Create basic markdown editor

---
+++ fyne-md/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+type config struct {
+	EditWidget    *widget.Entry
+	PreviewWidget *widget.RichText
+	CurrentFile   fyne.URI
+	SaveMenuItem  *fyne.MenuItem
+}
+
+var cfg config
+
+func main() {
+	// create a fyne app
+	a := app.New()
+
+	// create a window
+	w := a.NewWindow("Markdown")
+
+	// get the user interface
+	edit, preview := cfg.makeUI()
+
+	// set the content of the window
+	w.SetContent(container.NewHSplit(edit, preview))
+
+	// show window and run app
+	w.Resize(fyne.NewSize(800, 600))
+	w.CenterOnScreen()
+	w.ShowAndRun()
+}
+
+func (app *config) makeUI() (*widget.Entry, *widget.RichText) {
+	edit := widget.NewMultiLineEntry()
+	preview := widget.NewRichTextFromMarkdown("")
+
+	app.EditWidget = edit
+	app.PreviewWidget = preview
+
+	// add event listener
+	edit.OnChanged = preview.ParseMarkdown
+
+	return edit, preview
+}
