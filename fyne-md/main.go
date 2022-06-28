76100c169c3a29b58e02c839ca2a585fc5576e95
Hyohwak Lee <crewe1746@naver.com>
2022-06-28 19:47:07 +0900

    Enable to save the file by specifying the file name

--- fyne-md/main.go
+++ fyne-md/main.go
@@ -4,6 +4,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -52,11 +53,44 @@ func (app *config) makeUI() (*widget.Entry, *widget.RichText) {
 func (app *config) createMenuItems(w fyne.Window) {
 	openMenuItem := fyne.NewMenuItem("Open...", func() {})
 	saveMenuItem := fyne.NewMenuItem("Save", func() {})
-	saveAsMenuItem := fyne.NewMenuItem("Save as", func() {})
+
+	// disactivate saveMenuItem
+	app.SaveMenuItem = saveMenuItem
+	app.SaveMenuItem.Disabled = true
+
+	saveAsMenuItem := fyne.NewMenuItem("Save as", app.saveAsFunc(w))
 
 	fileMenu := fyne.NewMenu("File", openMenuItem, saveMenuItem, saveAsMenuItem)
 
 	menu := fyne.NewMainMenu(fileMenu)
 
 	w.SetMainMenu(menu)
+}
+
+func (app *config) saveAsFunc(w fyne.Window) func() {
+	return func() {
+		saveDialog := dialog.NewFileSave(func(uc fyne.URIWriteCloser, err error) {
+			if err != nil {
+				dialog.ShowError(err, w)
+				return
+			}
+
+			if uc == nil {
+				// user cancelled
+				return
+			}
+
+			// save file
+			uc.Write([]byte(app.EditWidget.Text))
+			app.CurrentFile = uc.URI()
+
+			defer uc.Close()
+
+			w.SetTitle("Markdown - " + uc.URI().Name())
+
+			app.SaveMenuItem.Disabled = false
+		}, w)
+
+		saveDialog.Show()
+	}
 }
