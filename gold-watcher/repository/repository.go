21e4d29a0f5a96905c5af30564e6b07d0b9af111
Hyohwak Lee <crewe1746@naver.com>
2022-07-04 07:52:52 +0900

    Setup database interface

---
+++ gold-watcher/repository/repository.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"errors"
+	"time"
+)
+
+var (
+	errUpdateFailed = errors.New("update failed")
+	errDeleteFailed = errors.New("delete failed")
+)
+
+type Repository interface {
+	Migrate() error
+	InsertHolding(h Holding) (*Holding, error)
+	AllHoldings() ([]Holding, error)
+	GetHoldingByID(id int) (*Holding, error)
+	UpdateHolding(id int64, updated Holding) error
+	DeleteHolding(id int64) error
+}
+
+type Holding struct {
+	ID            int64     `json:"id"`
+	Amount        int       `json:"amount"`
+	PurchaseDate  time.Time `json:"purchase_date"`
+	PurchasePrice int       `json:"purchase_price"`
+}
